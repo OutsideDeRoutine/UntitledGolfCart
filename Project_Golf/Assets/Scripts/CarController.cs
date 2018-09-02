2e4623e75a6b8b8341a4926b54124ea4aef877f7
Moisés Muñiz Mangas <36801152+moemmia@users.noreply.github.com>
2018-09-02 23:10:56 +0200

    Initial commit

---
+++ Project_Golf/Assets/Scripts/CarController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor;
+    public bool steering;
+}
+
+public class CarController : MonoBehaviour {
+
+    public List<AxleInfo> axleInfos;
+    public Transform steeringWheel;
+    public float maxMotorTorque;
+    public float maxSteeringAngle;
+
+
+    // finds the corresponding visual wheel
+    // correctly applies the transform
+    public void ApplyLocalPositionToVisuals(WheelCollider collider)
+    {
+        if (collider.transform.childCount == 0)
+        {
+            return;
+        }
+
+        Transform visualWheel = collider.transform.GetChild(0);
+
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;
+    }
+
+    private float actSWRot;
+    public void ApplySteeringWheelRot(float rotation)
+    {
+        float rotateNow= rotation - actSWRot;
+
+        steeringWheel.transform.RotateAround(steeringWheel.position, steeringWheel.forward, rotateNow*3);
+
+        actSWRot = rotation;
+    }
+
+    private float steering;
+    public void FixedUpdate()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+
+        float vel = GetComponent<Rigidbody>().velocity.magnitude;
+
+        float maxSteeringAtVel = Mathf.Clamp(maxSteeringAngle - (vel),10, maxSteeringAngle);
+
+        float h = Input.GetAxis("Horizontal")/2;
+
+        float fit = h ==0? (steering==0? 0:(steering>0? -vel : vel) / 20) : h * 4;
+        if (fit != h * 4)
+            steering += steering > 0 ? (steering + fit < 0 ? 0 : fit) : (steering + fit > 0 ? 0 : fit);
+        else steering += h * 4;
+
+        steering = Mathf.Clamp(steering, -maxSteeringAtVel, maxSteeringAtVel);
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+            ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+            ApplyLocalPositionToVisuals(axleInfo.rightWheel);
+        }
+
+        ApplySteeringWheelRot(steering);
+    }
+}
---
+++ Project_Golf/Assets/Scripts/SpectatorCamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpectatorCamera : MonoBehaviour {
+
+    [SerializeField] float mouseSensitivity=1;
+    [SerializeField] float velocity = 1;
+
+    private bool inUse;
+
+    void Update () {
+        MouseMove();
+        WasdMove();
+    }
+
+    private void MouseMove()
+    {
+        var h = Input.GetAxis("Mouse X");
+        var v = Input.GetAxis("Mouse Y");
+        transform.RotateAround(transform.position, transform.right, -v * mouseSensitivity);
+        transform.RotateAround(transform.position, Vector3.up, h * mouseSensitivity);
+    }
+
+    private void WasdMove()
+    {
+        if (Input.GetKey(KeyCode.W)) transform.Translate(Vector3.forward * 0.1f * velocity, Space.Self);
+        if (Input.GetKey(KeyCode.A)) transform.Translate(Vector3.left * 0.1f * velocity, Space.Self);
+        if (Input.GetKey(KeyCode.S)) transform.Translate(Vector3.back * 0.1f * velocity, Space.Self);
+        if (Input.GetKey(KeyCode.D)) transform.Translate(Vector3.right * 0.1f * velocity, Space.Self);
+    }
+}
