5e120efa7f71c6cdf92be92c00d0f15f1a2c8c98
Moisés Muñiz Mangas <36801152+moemmia@users.noreply.github.com>
2018-09-04 01:29:17 +0200

    mejoras sistema de conduccion + problemas a solucionar

--- Project_Golf/Assets/Scripts/CarController.cs
+++ Project_Golf/Assets/Scripts/CarController.cs
@@ -14,12 +14,21 @@ public class AxleInfo
 
 public class CarController : MonoBehaviour {
 
+    public enum CarState { Stopped, Forwards, Backwards };
+    public Transform CoM;
     public List<AxleInfo> axleInfos;
     public Transform steeringWheel;
     public float maxMotorTorque;
     public float maxSteeringAngle;
     public float aceleration;
+    public float brake;
 
+    public CarState state;
+
+    private void Start()
+    {
+        GetComponent<Rigidbody>().centerOfMass = CoM.localPosition;
+    }
 
     // finds the corresponding visual wheel
     // correctly applies the transform
@@ -53,20 +62,29 @@ public class CarController : MonoBehaviour {
     private float steering;
     public void FixedUpdate()
     {
-        float motor = maxMotorTorque * Input.GetAxis("Vertical") * aceleration;
+        Vector3 velocity = GetComponent<Rigidbody>().velocity;
 
-        float vel = GetComponent<Rigidbody>().velocity.magnitude;
+        Debug.DrawRay(CoM.position, velocity, Color.red);
+        Debug.DrawRay(CoM.position, CoM.forward);
 
-        float maxSteeringAtVel = Mathf.Clamp(maxSteeringAngle - (vel),10, maxSteeringAngle);
+        float v = Input.GetAxis("Vertical");
+        float motoraceleration = maxMotorTorque * v * aceleration;
+        float motorbreak = maxMotorTorque * Mathf.Abs(v) * brake;
 
-        float h = Input.GetAxis("Horizontal")/4;
 
-        float fit = h ==0? (steering==0? 0:(steering>0? -vel : vel) / 5) : h * 4;
+        float vel = velocity.magnitude;
+        float maxSteeringAtVel = Mathf.Clamp(maxSteeringAngle - (vel), 10, maxSteeringAngle);
+
+        float h = Input.GetAxis("Horizontal") / 4;
+
+        float fit = h == 0 ? (steering == 0 ? 0 : (steering > 0 ? -vel : vel) / 2) : h * 4;
         if (fit != h * 4)
-            steering += steering > 0 ? (steering + fit < 0 ? 0 : fit) : (steering + fit > 0 ? 0 : fit);
+            steering += steering > 0 ? (steering + fit < 0 ? -steering : fit) : (steering + fit > 0 ? -steering : fit);
         else steering += h * 4;
 
         steering = Mathf.Clamp(steering, -maxSteeringAtVel, maxSteeringAtVel);
+
+        UpdateCarState(CoM.position, CoM.forward , velocity);
 
         foreach (AxleInfo axleInfo in axleInfos)
         {
@@ -75,15 +93,85 @@ public class CarController : MonoBehaviour {
                 axleInfo.leftWheel.steerAngle = steering;
                 axleInfo.rightWheel.steerAngle = steering;
             }
+
+
             if (axleInfo.motor)
             {
-                axleInfo.leftWheel.motorTorque = motor;
-                axleInfo.rightWheel.motorTorque = motor;
+                axleInfo.leftWheel.motorTorque = motoraceleration;
+                axleInfo.rightWheel.motorTorque = motoraceleration;
+                if (axleInfo.rightWheel.motorTorque>0)
+                switch (state)
+                {
+                    case (CarState.Stopped):
+
+                        break;
+
+                    case (CarState.Forwards):
+                            axleInfo.leftWheel.motorTorque -= 100;
+                            axleInfo.rightWheel.motorTorque -= 100;
+                        break;
+
+                    case (CarState.Backwards):
+                            axleInfo.leftWheel.motorTorque += 150;
+                            axleInfo.rightWheel.motorTorque += 150;
+                        break;
+                }
+                
             }
+            else
+            {
+                axleInfo.leftWheel.brakeTorque = 0;
+                axleInfo.rightWheel.brakeTorque = 0;
+                switch (state)
+                {
+                    case (CarState.Stopped):
+
+                        break;
+
+                    case (CarState.Forwards):
+                        if (v < 0)
+                        {
+                            axleInfo.leftWheel.brakeTorque = motorbreak;
+                            axleInfo.rightWheel.brakeTorque = motorbreak;
+                        }
+                        break;
+
+                    case (CarState.Backwards):
+                        if (v > 0)
+                        {
+                            axleInfo.leftWheel.brakeTorque = motorbreak;
+                            axleInfo.rightWheel.brakeTorque = motorbreak;
+                        }
+                        break;
+                }
+            }
+
             ApplyLocalPositionToVisuals(axleInfo.leftWheel);
             ApplyLocalPositionToVisuals(axleInfo.rightWheel);
         }
 
         ApplySteeringWheelRot(steering);
+    }
+
+
+    private void UpdateCarState(Vector3 position,Vector3 direction, Vector3 velocity)
+    {
+        
+        if (Mathf.Abs( Vector3.Distance(Vector3.zero, velocity))< 0.01f)
+            state = CarState.Stopped;
+        else
+        {
+            float velAngle = Vector3.Angle(position, velocity);
+            float dirAngle = Vector3.Angle(position, direction);
+            Debug.Log(velAngle - dirAngle);
+            if (Mathf.Abs(velAngle- dirAngle)> maxSteeringAngle) // <---- PROBLEMAS
+            {
+                state = CarState.Backwards;
+            }
+            else
+            {
+                state = CarState.Forwards;
+            }
+        }
     }
 }
