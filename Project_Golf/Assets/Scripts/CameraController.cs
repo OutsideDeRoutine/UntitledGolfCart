4cd66bece834eab386a6dacbee7107437c74b3c1
Moisés Muñiz Mangas <36801152+moemmia@users.noreply.github.com>
2018-09-09 16:56:02 +0200

    simple camera
    [TODO] smooth rotations

---
+++ Project_Golf/Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    [SerializeField] float mouseSensitivity = 1;
+    [SerializeField] float velocity = 1;
+    public Transform LookAtMe;
+
+    private bool inUse;
+
+    void Update()
+    {
+        MouseMove();
+    }
+
+    private void MouseMove()
+    {
+        var h = Input.GetAxis("Mouse X");
+
+        var v = Input.GetAxis("Mouse Y");
+        transform.RotateAround(LookAtMe.position, transform.right, -v * mouseSensitivity);
+        
+        this.GetComponentInParent<CharControlller>().rotate(h);
+    }
+
+}
--- Project_Golf/Assets/Scripts/CharControlller.cs
+++ Project_Golf/Assets/Scripts/CharControlller.cs
@@ -10,6 +10,12 @@ public class CharControlller : MonoBehaviour {
     private CharacterController _controller;
     private Animator _animator;
 
+    [SerializeField]
+    private Transform Armature;
+
+
+    private float v;
+    private float h;
     void Start()
     {
         _controller = GetComponent<CharacterController>();
@@ -18,18 +24,30 @@ public class CharControlller : MonoBehaviour {
 
     void Update()
     {
-        float v = Input.GetAxis("Vertical");
-        float h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        h = Input.GetAxis("Horizontal");
         float gravity = -9.81f * Time.deltaTime * Mass;
         if (_controller.isGrounded)
         {
             gravity = 0;
         }
-        Debug.Log(_controller.isGrounded);
-        Vector3 move = this.transform.forward* v + this.transform.up * gravity;
-        _controller.Move(move * Time.deltaTime * Speed);
+        Vector3 move = this.transform.forward* v + this.transform.right *h;
+        _controller.Move(move.normalized * Time.deltaTime * Speed + Vector3.up * gravity);
+
+        _animator.SetInteger("walking", v>0? 1:v==0? h==0? 0: 1: -1);        
+    }
+
+    void LateUpdate()
+    {
+        Vector3 dir=_controller.velocity.normalized;
+        Debug.DrawRay(_controller.transform.position, dir,Color.red);
+        float t = Mathf.Abs(v) <= 0.1 ? 0 : v > 0 ? 1 : -1;
+        float angle = h == 0 ? 0 : t == 0 ? h * 90 : h * t * 45;
+        Armature.Rotate(Vector3.forward * Time.deltaTime, angle);
+    }
+
+    public void rotate(float h)
+    {
         this.transform.Rotate(this.transform.up, h * RotSpeed);
-        _animator.SetInteger("walking", v>0? 1:v==0? 0: -1);
-        
     }
 }
