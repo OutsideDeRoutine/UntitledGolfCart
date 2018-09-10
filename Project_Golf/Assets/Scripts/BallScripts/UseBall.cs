b4f2548bf261ea38f4dd0250880da805f51ee97e
Fernando Palazuelo <DatFerPa@users.noreply.github.com>
2018-09-11 00:45:06 +0200

    empezando disparo de bola
    Poco cosa para empezar con el lanzamiento de la pelota

---
+++ Project_Golf/Assets/Scripts/BallScripts/UseBall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseBall : AbstractUsable {
+
+
+    public Transform CharPosGolpeo;
+	
+	void Update () {
+		
+	}
+
+
+    public override void OnStart()
+    {
+        user.transform.position = CharPosGolpeo.position;
+        Debug.Log("Se ha colocado en la posicion de golpeo");
+
+    }
+
+
+    public override void OnEnd()
+    {
+
+
+
+
+    }
+}
