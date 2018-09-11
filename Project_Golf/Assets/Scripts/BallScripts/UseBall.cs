9500a555ac1c3f45f1c53ba71699cf9f976457e4
Moisés Muñiz Mangas <36801152+moemmia@users.noreply.github.com>
2018-09-11 20:15:01 +0200

    smoothie

--- Project_Golf/Assets/Scripts/BallScripts/UseBall.cs
+++ Project_Golf/Assets/Scripts/BallScripts/UseBall.cs
@@ -49,7 +49,7 @@ public class UseBall : AbstractUsable {
 
         Ball.GetComponent<Rigidbody>().AddTorque(torque, ForceMode.Impulse);
 
-        yield return new WaitUntil(() => user.GetComponent<CharControlller>().AnimationState("Swing") >= 1);
+        yield return new WaitUntil(() => user.GetComponent<CharControlller>().AnimationState("Swing") >= 0.8);
         //  3. CAMARA SEGUIR BOLA HASTA QUE ATERRIZA Y SE PARA
 
         yield return new WaitUntil(() => WhileBallMoving());
@@ -67,9 +67,22 @@ public class UseBall : AbstractUsable {
         InUse = false;
     }
 
+    public float smoothCamPos = 0.2f;
+    public float smoothCamRot = 0.3f;
+    private Vector3 velocity = Vector3.zero;
+    private Transform cam;
+
+    void Start()
+    {
+        cam = Camera.main.transform;
+    }
     public bool WhileBallMoving()
     {
-        Camera.main.transform.position = Ball.transform.position - this.transform.forward / 2 + Vector3.up/10;
+        cam.position = Vector3.SmoothDamp(Camera.main.transform.position , Ball.transform.position - this.transform.forward / 2 + Vector3.up/10, ref velocity, smoothCamPos);
+        Quaternion rot = cam.rotation;
+        cam.LookAt(Ball.transform.position);
+        cam.rotation = Quaternion.RotateTowards(rot, cam.transform.rotation,Time.time * smoothCamRot);
+
         return Ball.GetComponent<Rigidbody>().velocity.magnitude < 0.02f;
     }
 
