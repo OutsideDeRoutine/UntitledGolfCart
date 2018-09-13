48b081af31045a55bb603f2d2470d038084f96d6
Moisés Muñiz Mangas <36801152+moemmia@users.noreply.github.com>
2018-09-13 16:51:34 +0200

    fix UI

--- Project_Golf/Assets/Scripts/BallScripts/UIPointBall.cs
+++ Project_Golf/Assets/Scripts/BallScripts/UIPointBall.cs
@@ -33,8 +33,21 @@ public class UIPointBall : MonoBehaviour {
         Vector3 v3Pos = Camera.main.WorldToViewportPoint(ball.transform.position);
         float fAngle;
 
+        if (v3Pos.z < Camera.main.nearClipPlane)
+        {
+            v3Pos.x -= 0.5f;  // Translate to use center of viewport
+            v3Pos.y -= 0.5f;
+            v3Pos.z = 0;
 
-        if (v3Pos.x >= 0.0f && v3Pos.x <= 1.0f && v3Pos.y >= 0.0f && v3Pos.y <= 1.0f) // Object center is visible
+            fAngle = Mathf.Atan2(v3Pos.x, v3Pos.y);
+            transform.localEulerAngles = new Vector3(0.0f, 0.0f, -fAngle * Mathf.Rad2Deg);
+
+            transform.localPosition = Vector3.zero - this.transform.up * 100;
+            return;
+        }
+
+
+       if (v3Pos.x >= 0.0f && v3Pos.x <= 1.0f && v3Pos.y >= 0.0f && v3Pos.y <= 1.0f) // Object center is visible
         {
 
             transform.localEulerAngles = new Vector3(0.0f, 0.0f, 0f);
@@ -42,17 +55,14 @@ public class UIPointBall : MonoBehaviour {
             transform.position = cam.WorldToScreenPoint(ball.transform.position + (cam.transform.up/2));
             return;
         }
-        else
-        {
-            int s= v3Pos.z < Camera.main.nearClipPlane? 0:180;
+
             v3Pos.x -= 0.5f;  // Translate to use center of viewport
             v3Pos.y -= 0.5f;
             v3Pos.z = 0;
 
             fAngle = Mathf.Atan2(v3Pos.x, v3Pos.y);
-            transform.localEulerAngles = new Vector3(0.0f, 0.0f, -fAngle * Mathf.Rad2Deg + s);
+            transform.localEulerAngles = new Vector3(0.0f, 0.0f, -fAngle * Mathf.Rad2Deg + 180);
 
             transform.localPosition = Vector3.zero - this.transform.up * 100;
-        }
     }
 }
