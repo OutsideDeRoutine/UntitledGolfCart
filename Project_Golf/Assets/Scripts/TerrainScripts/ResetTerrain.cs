9cc0d3feb7c8ebad7248b59ebe8ef01e559104ed
Moisés Muñiz Mangas <36801152+moemmia@users.noreply.github.com>
2018-10-03 01:14:34 +0200

    test ajuste de terreno

---
+++ Project_Golf/Assets/Scripts/TerrainScripts/ResetTerrain.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetTerrain : MonoBehaviour {
+
+    public Terrain Terrain;
+
+    private float[,] originalHeights;
+
+    private void OnDestroy()
+    {
+        this.Terrain.terrainData.SetHeights(0, 0, this.originalHeights);
+    }
+
+    private void Awake()
+    {
+        this.originalHeights = this.Terrain.terrainData.GetHeights(
+            0, 0, this.Terrain.terrainData.heightmapWidth, this.Terrain.terrainData.heightmapHeight);
+    }
+
+}
